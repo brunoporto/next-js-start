6cdfa11e465d7e6fa75cac257d780cfa2239d026
Bruno Porto <brunotporto@gmail.com>
2020-09-08 20:15:50 -0300

    ajuste do projeto de start

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import "reset-css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "react-image-gallery/styles/css/image-gallery.css";
 
 import App from "next/app";
 import Head from "next/head";
@@ -14,49 +13,12 @@ class MyApp extends App {
     },
   };
 
-  // componentDidMount = () => {
-  //   window.addEventListener("scroll", this.handleScroll, true);
-  // };
-
-  // componentWillUnmount = () => {
-  //   window.removeEventListener("scroll", this.handleScroll);
-  // };
-
-  // handleScroll = () => {
-  //   // console.log(document.body.scrollTop);
-  //   if (
-  //     document.body.scrollTop > 150 ||
-  //     document.documentElement.scrollTop > 150
-  //   ) {
-  //     document.getElementById("top-menu-nav").classList.add("shrink");
-  //   } else {
-  //     document.getElementById("top-menu-nav").classList.remove("shrink");
-  //   }
-  // };
-
   render() {
     const { Component, pageProps } = this.props;
     return (
       <>
         <Head>
           <title>ProjectStart</title>
-          {/* <link
-            rel="shortcut icon"
-            href="/images/petcarioca.oficial.jpg"
-            type="image/x-icon"
-          />
-          <link
-            rel="stylesheet"
-            id="et-builder-googlefonts-cached-css"
-            href="https://fonts.googleapis.com/css?family=Quicksand%3A300%2Cregular%2C500%2C700&#038;ver=5.4.2#038;subset=latin,latin-ext"
-            type="text/css"
-            media="all"
-          />
-          <link
-            href="https://fonts.googleapis.com/css?family=Verdana&v1"
-            rel="stylesheet"
-            type="text/css"
-          /> */}
         </Head>
         <ConfigContext.Provider value={{ config: this.state.config }}>
           <Component {...pageProps} />
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,16 +1,7 @@
 import PageContainer from "../components/page-container";
 
 function Home() {
-  return (
-    <PageContainer>
-      {/* <HomeInner></HomeInner>
-      <CamevInner></CamevInner>
-      <PetStoreInner></PetStoreInner>
-      <EsteticaAnimalInner></EsteticaAnimalInner>
-      <SigaNosInner></SigaNosInner>
-      <FaleConoscoInner></FaleConoscoInner> */}
-    </PageContainer>
-  );
+  return <PageContainer></PageContainer>;
 }
 
 export default Home;
